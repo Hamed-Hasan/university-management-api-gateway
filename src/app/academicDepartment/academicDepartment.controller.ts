baa9fe1ec1d860e818f68175cf0b69f279bf0fa9
John Doe <hamedhasan422421@gmail.com>
2023-09-24 09:40:10 +0300

    📦 Refactor Academic Department Controllers: • Extract common logic to service layer. • Implement async/await for better error handling. 🐞

---
+++ src/app/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import sendResponse from '../../../shared/response';
+import { AcademicDepartmentService } from './academicDepartment.service';
+
+const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicDepartmentService.getAllFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const insertIntoDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicDepartmentService.insertIntoDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicDepartmentService.getByIdFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
