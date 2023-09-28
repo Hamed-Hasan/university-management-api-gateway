9d5037793c2f2b5e733ebd3197651fd3ffc224cf
John Doe <hamedhasan422421@gmail.com>
2023-09-28 14:45:56 +0300

    🚀 Added getAllFromDB function. ✨ Implemented getByIdFromDB. 📦 Added insertIntoDB function. 🩺 Refactored code in getAllFromDB. 🛠️ Minor improvements in getByIdFromDB.

---
+++ src/app/modules/building/building.service.ts
@@ -0,0 +1,33 @@
+import { Request } from 'express';
+import { IGenericResponse } from '../../../interfaces/common';
+import { CoreService } from '../../../shared/axios';
+
+const getAllFromDB = async (req: Request): Promise<IGenericResponse> => {
+  console.log(req);
+  const response: IGenericResponse = await CoreService.get('/buildings', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const getByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await CoreService.get(`/buildings/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const insertIntoDB = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await CoreService.post(`/buildings`, req.body, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
